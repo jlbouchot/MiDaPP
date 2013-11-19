0ec374e3092cc939738a8dc47e1fdeaea7e8b34a
Calvin Morrison <mutantturkey@gmail.com>
2013-11-19 16:32:29 -0500

    cleanups

--- output_missing_data_matrix.py
+++ output_missing_data_matrix.py
@@ -16,22 +16,34 @@ __maintainer__ = "Calvin Morrison"
 __email__ = "mutantturkey@gmail.com"
 __status__ = "Development"
 
-def save_biom_matrix(input_file, output_file):
+def save_discrete_mapping(discrete_file, filetype, obs_dic): 
+	
+	if(filetype == 'csv'):
+		with open(discrete_file, 'w') as fh:
+			fh.write("#KEY\tMAPPED_VAL\tORIGINAL_VAL\n")
+			for m in obs_dic.keys():
+				for i in obs_dic[m].items():
+					fh.write(m + "\t" + str(i[0]) + "\t" + str(i[1]) + "\n")
 
-	with open(input_file, 'r') as ih:
-		with open(output_file, 'w') as oh:
-			biom_table = parse_biom_table(ih)
-			matrix, site_names, variable_names = biom_table_to_array(biom_table)
-			oh.write("Samples\t")
-			for x in xrange(len(matrix[0])):
-				oh.write(site_names[x] + "\t")
-			oh.write("\n")
-			for x in xrange(len(matrix)):
-				oh.write(variable_names[x] + "\t")
-				for y in xrange(len(matrix[x])):
-					oh.write(str(matrix[x][y]) + "\t")
-				oh.write("\n")
-		
+def save_biom_matrix_csv(input_file, output_file):
+
+	ih = open(input_file, 'r')
+	biom_table = parse_biom_table(ih)
+
+	oh = open(output_file, 'w')
+	matrix, site_names, variable_names = biom_table_to_array(biom_table)
+
+	oh.write("Samples\t")
+	for x in xrange(len(matrix[0])):
+		oh.write(site_names[x] + "\t")
+	oh.write("\n")
+
+	for x in xrange(len(matrix)):
+		oh.write(variable_names[x] + "\t")
+		for y in xrange(len(matrix[x])):
+			oh.write(str(matrix[x][y]) + "\t")
+		oh.write("\n")
+
 def biom_table_to_array(biom_table):
 	"""
 		biom_table_to_array(biom_table)
@@ -42,49 +54,74 @@ def biom_table_to_array(biom_table):
 	"""
 	site_names = list(biom_table.SampleIds)
 	variable_names = list(biom_table.ObservationIds)
-	data_matrix = [] 
+
 	data_matrix = [o for o in biom_table.iterObservationData()] 
+
 	return data_matrix, site_names, variable_names
 		
 
 
+def load_features_of_interest(foi_file):
+
+	features = {}
+	with open(foi_file, 'r') as fh:
+		for i,row in enumerate(fh):
+			# -1 to get rid of the newline
+			split_row = row[:-1].split("\t")
+			# Make sure we have 3 rows.
+			if(len(split_row) != 3):
+				raise("Three rows are required in your feature of interest file. Error row: '" + row[:-1] + "'")
+
+			# populate our features of interest dict
+			feature_name = split_row[0]
+			discrete_val = split_row[1]
+			type_val = split_row[2]
+
+			features[feature_name] = {}
+			features[feature_name]['discrete'] = discrete_val
+			features[feature_name]['type'] = type_val 
+	return features
+
+
+def load_data_dic(mapping_file):
+	missing_data_dic = {}
+
+	with open(mapping_file, 'r') as fh:
+		header_line = fh.readline()	
+		# use 1:-1 for the first # and the newline
+		headers = header_line[1:-1].split("\t")
+
+		for row in fh:
+			columns = row[:-1].split("\t")
+			sample_name =	columns[0]
+
+			if sample_name not in missing_data_dic:
+				missing_data_dic[sample_name] = {}
+
+			for i, col in enumerate(columns):
+				missing_data_dic[sample_name][headers[i]] = col 
+
+	return missing_data_dic
+
 def save_desired_features(mapping_file, foi_file, output_file, discrete_file):
 		features = {}
 		missing_data_dic = {}
-		output_array = []
+		data_array = []
+		obs_dic = {}
 
-		output_discrete_fh = open(discrete_file, 'w');
+		features = load_features_of_interest(foi_file);
+		missing_data_dic = load_data_dic(mapping_file)
 
-		with open(foi_file, 'r') as fh:
-			for i,row in enumerate(fh):
-				split_row = row[:-1].split("\t")
-				features[split_row[0]] = {}
-				features[split_row[0]]['discrete'] = split_row[1]
-				features[split_row[0]]['type'] = split_row[2]
-
-		with open(mapping_file, 'r') as fh:
-			header_line = fh.readline()	
-			headers = header_line[1:-1].split("\t")
-
-			for row in fh:
-				columns = row.split("\t")
-				sample_name =	columns[0]
-
-				if sample_name not in missing_data_dic:
-					missing_data_dic[sample_name] = {}
-
-				for i, col in enumerate(columns):
-						missing_data_dic[sample_name][headers[i]] = col 
 
 		# for each sample, add our desired data to an array
-		output_array.append([])
-		output_array[0].append("Sample")
+		data_array.append([])
+		data_array[0].append("Sample")
 		for feature in features:
-			output_array[0].append(feature)
+			data_array[0].append(feature)
 
 		for x, sample in list(enumerate(missing_data_dic.keys()))[1:]:
-			output_array.append([])
-			output_array[x].append(sample)
+			data_array.append([])
+			data_array[x].append(sample)
 
 			# for each desired feature, add it from the missing_data_dictionary
 			for desired_feature in features.keys():
@@ -101,14 +138,13 @@ def save_desired_features(mapping_file, foi_file, output_file, discrete_file):
 						elif desired_feature[2].lower() == "i":
 							value = int(value)
 					
-						output_array[x].append(value)
+						data_array[x].append(value)
 
 		# transpose our output array
-		output_array = map(list, zip(*output_array))
+		data_array = map(list, zip(*data_array))
 
-		# replace our value which are supposed to be discete with descrete values
-		obs_dic = {}
-		for f, feature in list(enumerate(output_array[1:]))[1:]:
+		# replace our value which are supposed to be discete with discrete values
+		for f, feature in list(enumerate(data_array[1:])):
 			if(features[feature[0]]['discrete'] == "discrete"):
 
 				obs_dic[feature[0]] = {}
@@ -121,21 +157,15 @@ def save_desired_features(mapping_file, foi_file, output_file, discrete_file):
 					feature[x] = obs_dic[feature[0]][feature[x]]
 
 		# write our observation dictionary out
-		with open(discrete_file, 'w') as fh:
-				
-			fh.write("#KEY\tMAPPED_VAL\tORIGINAL_VAL\n")
-			for m in obs_dic.keys():
-				for i in obs_dic[m].items():
-					fh.write(m + "\t" + str(i[0]) + "\t" + str(i[1]) + "\n")
+		
+		save_discrete_mapping(discrete_file, 'csv', obs_dic);
 
-
-		output_array = map(list, zip(*output_array))
+		data_array = map(list, zip(*data_array))
 		with open(output_file, 'w') as fh:
-			for row in output_array:
+			for row in data_array:
 				for col in row[:-1]:
 					fh.write(str(col) + "\t")
 				fh.write(str(row[len(row) - 1]) + "\n")
-
 
 def main():
 		parser = argparse.ArgumentParser(description = "")
@@ -159,7 +189,7 @@ def main():
 													args.output_folder + "/desired_features.csv",
 													args.output_folder + "/discrete_feature_mapping.csv")
 
-		save_biom_matrix(args.biom_file, args.output_folder + "/data_matrix.csv")
+		save_biom_matrix_csv(args.biom_file, args.output_folder + "/data_matrix.csv")
 
 		# output our data matrix
 		# if args.output_type == "csv":
